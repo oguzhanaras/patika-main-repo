a0ee08c55f07e070e5fcef2f6df81c129bbb8fcc
oguzhanaras <oguzhanarasx@gmail.com>
2024-07-16 23:24:37 +0300

    new project drum kit
    my new project mini drumkit page with javascript css and update link index.html

---
+++ javascript-drumkit/README.md
@@ -0,0 +1,2 @@
+javascript ile mini drum-kit uygulaması
+live page link: https://oguzhanaras.github.io/patika-main-repo/javascript-drumkit/
---
+++ javascript-drumkit/script.js
@@ -0,0 +1,85 @@
+let a = document.getElementById("a");
+let s = document.getElementById("s");
+let d = document.getElementById("d");
+let f = document.getElementById("f");
+let g = document.getElementById("g");
+let h = document.getElementById("h");
+let j = document.getElementById("j");
+let k = document.getElementById("k");
+let l = document.getElementById("l");
+
+
+document.body.addEventListener("keydown", (event) => {
+    let key = event.key;
+    let sound = document.getElementById(key + "-audio");
+    sound.play();
+    document.getElementById(key).style.backgroundColor = "yellow";
+    document.getElementById(key).style.boxShadow = "red 2px 2px";
+});
+
+document.body.addEventListener("keyup", (event) => {
+    let key = event.key;
+    document.getElementById(key).style.backgroundColor = "white";
+    document.getElementById(key).style.boxShadow = "none";
+});
+
+document.body.addEventListener("mousedown", (event) => {
+    let key = event.target.id;
+    let sound = document.getElementById(key + "-audio");
+    sound.play();
+    document.getElementById(key).style.backgroundColor = "yellow";
+    document.getElementById(key).style.boxShadow = "red 2px 2px";
+});
+
+document.body.addEventListener("mouseup", (event) => {
+    let key = event.target.id;
+    console.log(key);
+    document.getElementById(key).style.backgroundColor = "white";
+    document.getElementById(key).style.boxShadow = "none";
+});
+
+
+// a.addEventListener("click", () => {
+//     let sound = document.getElementById("a-audio");
+//     sound.play();
+// });
+
+// s.addEventListener("click", () => {
+//     let sound = document.getElementById("s-audio");
+//     sound.play();
+// });
+
+// d.addEventListener("click", () => {
+//     let sound = document.getElementById("d-audio");
+//     sound.play();
+// });
+
+// f.addEventListener("click", () => {
+//     let sound = document.getElementById("f-audio");
+//     sound.play();
+// });
+
+// g.addEventListener("click", () => {
+//     let sound = document.getElementById("g-audio");
+//     sound.play();
+// });
+
+// h.addEventListener("click", () => {
+//     let sound = document.getElementById("h-audio");
+//     sound.play();
+// });
+
+// j.addEventListener("click", () => {
+//     let sound = document.getElementById("j-audio");
+//     sound.play();
+// });
+
+// k.addEventListener("click", () => {
+//     let sound = document.getElementById("k-audio");
+//     sound.play();
+// });
+
+// l.addEventListener("click", () => {
+//     let sound = document.getElementById("l-audio");
+//     sound.play();
+// });
