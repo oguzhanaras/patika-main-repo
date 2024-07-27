b2c5330b15863aa0e335d9da7ce8be701ac0207c
oguzhanaras <oguzhanarasx@gmail.com>
2024-07-27 23:10:01 +0300

    apple project
    camera section and css

---
+++ apple/js/main.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    var camera = new Swiper('#camera .swiper', {
+        speed: 600,
+        spaceBetween: 12,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    })
+})();
